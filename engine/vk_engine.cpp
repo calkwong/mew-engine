7d3fc3fb4ae97c2d66a08c669e5276581526bd0e
calkwong <75272424+calkwong@users.noreply.github.com>
2026-03-25 01:31:58 +0800

    backface + zero area triangle culling in vbuffer path
     50% reduction in clipping invocations

--- engine/vk_engine.cpp
+++ engine/vk_engine.cpp
@@ -57,6 +57,7 @@ AutoCVar_Int CVAR_RENDER_LOD{ "render.lod", "LODs", 1, CVarFlags::EditCheckbox }
 AutoCVar_Int CVAR_RENDER_SHADOWS{ "render.shadows", "Shadows", 0, CVarFlags::EditCheckbox };
 AutoCVar_Int CVAR_RENDER_TAA{ "render.taa", "TAA", 0, CVarFlags::EditCheckbox | CVarFlags::EditHide };
 AutoCVar_Float CVAR_RENDER_MIN_CONTRIBUTION{ "render.meshlet_contribution", "Min meshlet contribution", 2.f, CVarFlags::EditDragFloat, 0.f, 4.f, 1.f };
+AutoCVar_Int CVAR_RENDER_TRIANGLE_CULLING{ "render.triangle_culling", "Triangle culling", 1, CVarFlags::EditCheckbox};
 
 AutoCVar_Int CVAR_SHADOWS_PCF{ "shadows.pcf", "PCF", 1, CVarFlags::EditCheckbox };
 AutoCVar_Float CVAR_SHADOWS_CASCADE_SPLIT{ "shadows.cascade_split", "Cascades log factor", 0.95f, CVarFlags::EditDragFloat, 0.f, 1.f, 0.005f };
@@ -3154,6 +3155,8 @@ void VulkanEngine::render(VkCommandBuffer cmd, bool late, uint32_t post_pass, ui
 		else
 			pc.jitter_offset -= glm::vec2(x, y);
 	}
+
+	pc.cull = CVAR_RENDER_TRIANGLE_CULLING.get();
 
 	if (!CVAR_RENDER_MESH_SHADERS.get())
 	{
