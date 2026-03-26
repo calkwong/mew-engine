a0d763eb1e454511273273cbe4da7b231fe58df6
calkwong <75272424+calkwong@users.noreply.github.com>
2026-03-26 22:58:48 +0800

    make triangle culling unconditional, except for deferred path

--- engine/vk_engine.cpp
+++ engine/vk_engine.cpp
@@ -57,7 +57,6 @@ AutoCVar_Int CVAR_RENDER_OCCLUSION_CULL{ "render.occlusion_cull", "Occlusion cul
 AutoCVar_Int CVAR_RENDER_LOD{ "render.lod", "LODs", 1, CVarFlags::EditCheckbox };
 AutoCVar_Int CVAR_RENDER_SHADOWS{ "render.shadows", "Shadows", 0, CVarFlags::EditCheckbox };
 AutoCVar_Int CVAR_RENDER_TAA{ "render.taa", "TAA", 0, CVarFlags::EditCheckbox | CVarFlags::EditHide };
-AutoCVar_Int CVAR_RENDER_TRIANGLE_CULLING{ "render.triangle_culling", "Triangle culling", 1, CVarFlags::EditCheckbox};
 
 AutoCVar_Int CVAR_SHADOWS_PCF{ "shadows.pcf", "PCF", 1, CVarFlags::EditCheckbox };
 AutoCVar_Float CVAR_SHADOWS_CASCADE_SPLIT{ "shadows.cascade_split", "Cascades log factor", 0.95f, CVarFlags::EditDragFloat, 0.f, 1.f, 0.005f };
@@ -3201,8 +3200,6 @@ void VulkanEngine::render(VkCommandBuffer cmd, bool late, uint32_t post_pass, ui
 		else
 			pc.jitter_offset -= glm::vec2(x, y);
 	}
-
-	pc.cull = CVAR_RENDER_TRIANGLE_CULLING.get();
 
 	if (!CVAR_RENDER_MESH_SHADERS.get())
 	{
