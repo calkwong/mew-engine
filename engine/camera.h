6f4248aa97ae1f564ad8baa860999ab6f558a3c1
calkwong <75272424+calkwong@users.noreply.github.com>
2026-05-04 01:15:28 +0800

    indirect multiscattering

--- engine/camera.h
+++ engine/camera.h
@@ -9,7 +9,7 @@ class Camera
 public:
 	glm::vec3 position{};
 	glm::vec3 velocity{};
-	float speed{ 2.f };
+	float speed{ 7.f };
 	float sensitivity{ 0.005f };
 	float pitch{ 0.0f };
 	float yaw{ 0.0f };
